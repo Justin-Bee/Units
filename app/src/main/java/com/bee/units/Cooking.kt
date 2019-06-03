2685e4fece91a45b44ff715444366490670ada7c
Justin Bee <jbee.appz@gmail.com>
2019-06-03 09:12:22 -0400

    Update Cooking.kt

--- app/src/main/java/com/bee/units/Cooking.kt
+++ app/src/main/java/com/bee/units/Cooking.kt
@@ -1,1 +1,134 @@
+package com.bee.units
 
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import kotlinx.android.synthetic.main.data.*
+
+
+
+class Cooking: Fragment(), View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+    var from:String=""
+    var to:String =""
+
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.abutton-> {
+                entry = textBox.text.toString()
+            }else->{
+        }
+        }
+        entry = textBox.text.toString()
+        val result = convert(entry)
+        textView.text = "Result: $result"
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        if(parent.getId()==(R.id.fromSpinner)) {
+            parent.getItemAtPosition(pos)
+            when (pos) {
+                0 -> from = "ts"
+                1 -> from = "tb"
+                2 -> from = "o"
+                3 -> from = "c"
+                4 -> from = "fo"
+                5 -> from = "p"
+                6 -> from = "q"
+                7 -> from = "g"     
+            }
+
+        }else if(parent.getId()==(R.id.toSpinner)){
+            parent.getItemAtPosition(pos)
+            when (pos) {
+                0 -> from = "ts"
+                1 -> from = "tb"
+                2 -> from = "o"
+                3 -> from = "c"
+                4 -> from = "fo"
+                5 -> from = "p"
+                6 -> from = "q"
+                7 -> from = "g" 
+            }
+        }
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        if(parent.getId()==R.id.fromSpinner) {
+            from = "c"
+        }else{
+            to ="f"
+        }
+    }
+
+
+    var entry: String = ""
+
+
+    companion object {
+        fun newInstance(): Cooking {
+            return Cooking()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        // Inflate the layout for this fragment
+        val view: View=  inflater.inflate(R.layout.cooking, container, false)
+        val button: Button = view.findViewById(R.id.abutton)
+        button.setOnClickListener(this)
+
+        val fromSpinner: Spinner = view.findViewById(R.id.fromSpinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        fromSpinner.adapter = ArrayAdapter(
+            activity,
+            R.layout.spinner_item,
+            resources.getStringArray(R.array.cooking_array)
+        )
+        val toSpinner: Spinner = view.findViewById(R.id.toSpinner)
+        toSpinner.adapter = ArrayAdapter(
+            activity,
+            R.layout.spinner_item,
+            resources.getStringArray(R.array.cooking_array)
+        )
+        fromSpinner.onItemSelectedListener= this
+        toSpinner.onItemSelectedListener = this
+
+        return view
+    }
+
+
+    fun convert(x: String):String{
+        try {
+            var num = Integer.parseInt(x)
+            var result=""
+            if (from.equals("ts") && to.equals("tb")){
+                result = celsiusToFahrenheit(num)
+          
+            }else if(from.equals(to)){
+                 result = "Please choose valid parameters"
+            }
+
+            return result.toString()
+        }catch(nfe: NumberFormatException){
+            return ("Invalid Input")
+        }
+    }
+
+    
+
+}
