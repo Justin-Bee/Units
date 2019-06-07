94fe07b288099ec631b2f8af211e9acb7e17007a
Justin Bee <jbee.appz@gmail.com>
2019-06-07 12:44:59 -0400

    Update Cooking.kt

--- app/src/main/java/com/bee/units/Cooking.kt
+++ app/src/main/java/com/bee/units/Cooking.kt
@@ -151,6 +151,7 @@ class Cooking: Fragment(), View.OnClickListener, AdapterView.OnItemSelectedListe
             }else if (from.equals("c") && to.equals("g")){
                 result = cupToGallons(num)
             }else if (from.equals("fo") && to.equals("ts")){
+                result = fluidOunceToTeaspoon(num)
             }else if (from.equals("fo") && to.equals("tb")){
             }else if (from.equals("fo") && to.equals("c")){
             }else if (from.equals("fo") && to.equals("p")){
@@ -257,6 +258,10 @@ class Cooking: Fragment(), View.OnClickListener, AdapterView.OnItemSelectedListe
 
     fun cupToGallons(x: Int):String {
         return (x / 16.0).toString()
+    }
+    
+    fun fluidOunceToTeaspoon(x: Int):String{
+        return (x*6.0).toString()
     }
 
 
